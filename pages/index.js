82f9c4a930a3ea5cdf96b5c0efef1d099f8dd48e
Donavon Dornan <donavondornan@MacBookPro.lan>
2025-07-17 09:32:33 -0600

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-black text-green-400 font-mono">
+      <h1 className="text-3xl">🤖 Welcome to Bot Bets</h1>
+    </div>
+  );
+}
