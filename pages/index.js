6df85832e55db4e07c3d323e1bdec0eeabb34678
LordWinnyG <youremail@example.com>
2025-07-17 10:45:08 -0600

    Fix logo file extension

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-black">
-      <img src="/botbets-logo.png" alt="Bot Bets Logo" className="w-1/2 max-w-xs" />
+      <img src="/botbets.jpg" alt="Bot Bets Logo" className="w-1/2 max-w-xs" />
     </div>
   );
 }
